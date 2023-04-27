de2654d55e02840f3dbfb64591652141990596d4
z4yross <z4yross@gmail.com>
2023-04-27 17:27:04 -0500

    descriptions

--- app/ciphers/Symmetric/Caesar.ts
+++ app/ciphers/Symmetric/Caesar.ts
@@ -1,14 +1,8 @@
 import { SymmetricCipher } from "../module.js";
 import { Alphabet, DefaultSingleton } from "../../module.js";
 
-const caesarDescription =
-	"In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.";
 
 export class Caesar implements SymmetricCipher {
-	static cipherName: string = "Caesar";
-	static description: string = caesarDescription;
-	static type: string = "Symmetric";
-
 	alphabet: Alphabet;
 	secret: string;
 	forward: boolean;
@@ -55,7 +49,6 @@ export class Caesar implements SymmetricCipher {
 			const resultNumbers = indexes.map((index) => {
 				if (this.forward) index -= shift;
 				else index += shift;
-				index -= shift;
 				index %= this.alphabet.getLength();
 				if (index < 0) index += this.alphabet.getLength();
 				return index;
--- app/ciphers/Symmetric/module.ts
+++ app/ciphers/Symmetric/module.ts
@@ -1,12 +1,12 @@
-import { Caesar } from './Caesar.js';
+import { Caesar } from "./Caesar.js";
 
-const ciphers = [ Caesar ];
+export const ciphers = [
+	{
+		cipherName: "Caesar",
+		description: "In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.",
+		type: "Symmetric",
+		cipher: Caesar,
+	},
+];
 
-export const ciphersDescriptions = ciphers.map((cipher) => {
-    return {
-        name: cipher.cipherName,
-        description: cipher.description,
-        type: cipher.type,
-        cipher: cipher
-    };
-});
+export * from "./Caesar.js";
