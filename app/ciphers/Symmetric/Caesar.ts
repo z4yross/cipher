7d96d4dec251f0aa474618c07f61981decc30eac
z4yross <z4yross@gmail.com>
2023-04-27 17:11:59 -0500

    descriptions

--- app/ciphers/Symmetric/Caesar.ts
+++ app/ciphers/Symmetric/Caesar.ts
@@ -1,17 +1,31 @@
 import { SymmetricCipher } from "../module.js";
 import { Alphabet, DefaultSingleton } from "../../module.js";
 
+const caesarDescription =
+	"In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.";
+
 export class Caesar implements SymmetricCipher {
+	static cipherName: string = "Caesar";
+	static description: string = caesarDescription;
+	static type: string = "Symmetric";
+
 	alphabet: Alphabet;
 	secret: string;
+	forward: boolean;
 
-	constructor(secret: string, alphabet?: Alphabet) {
-		this.alphabet = DefaultSingleton.getInstance();
+	constructor(
+		secret: string,
+		alphabet: Alphabet = DefaultSingleton.getInstance(),
+		forward: boolean = true
+	) {
+		this.alphabet = alphabet;
 
 		if (isNaN(+secret)) throw new Error("Secret must be a number");
 
 		this.secret = secret;
 		if (alphabet) this.alphabet = alphabet;
+
+		this.forward = forward;
 	}
 
 	encrypt(text: string): string {
@@ -20,7 +34,8 @@ export class Caesar implements SymmetricCipher {
 
 			const indexes = this.alphabet.indexString(text);
 			const resultNumbers = indexes.map((index) => {
-				index += shift;
+				if (this.forward) index += shift;
+				else index -= shift;
 				index %= this.alphabet.getLength();
 				return index;
 			});
@@ -38,6 +53,8 @@ export class Caesar implements SymmetricCipher {
 
 			const indexes = this.alphabet.indexString(text);
 			const resultNumbers = indexes.map((index) => {
+				if (this.forward) index -= shift;
+				else index += shift;
 				index -= shift;
 				index %= this.alphabet.getLength();
 				if (index < 0) index += this.alphabet.getLength();
--- app/ciphers/Symmetric/module.ts
+++ app/ciphers/Symmetric/module.ts
@@ -1,1 +1,12 @@
-export * from "./Caesar.js"
+import { Caesar } from './Caesar.js';
+
+const ciphers = [ Caesar ];
+
+export const ciphersDescriptions = ciphers.map((cipher) => {
+    return {
+        name: cipher.cipherName,
+        description: cipher.description,
+        type: cipher.type,
+        cipher: cipher
+    };
+});
